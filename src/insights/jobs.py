571e2852812505f06061265100180e620590f375
Nancy Schaal <nan_cy29@hotmail.com>
2024-06-04 19:13:18 +0200

    req 2,3,4

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,15 +1,38 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict]:
+        with open(path, encoding="utf-8") as file:
+            reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            data = list(reader)
+            self.jobs_list = data
 
-    def get_unique_job_types(self) -> List[str]:
-        pass
+        return self.jobs_list
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def get_unique_job_types(self):
+        job_types = set()
+        for job in self.jobs_list:
+
+            if 'job_type' in job and job['job_type']:
+                job_types.add(job['job_type'])
+        return list(job_types)
+
+    def filter_by_multiple_criteria(
+        self, jobs: List[Dict], filter_criteria: Dict
+    ) -> List[Dict]:
+        if not isinstance(filter_criteria, dict):
+            raise TypeError("filter_criteria must be a dictionary")
+
+        filtered_jobs = []
+        for job in jobs:
+            if all(
+                job.get(key) == value for key, value in filter_criteria.items()
+            ):
+                filtered_jobs.append(job)
+
+        return filtered_jobs
