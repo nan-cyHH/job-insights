e9d05982c037f6a1d793e2796910f4d298f37d93
Nancy Schaal <nan_cy29@hotmail.com>
2024-06-04 19:35:46 +0200

    req 6,7,8

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,15 +7,44 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        max_salary = 0
+        for job in self.jobs_list:
+            if "max_salary" in job and job["max_salary"].isdigit():
+                salary = int(job["max_salary"])
+                max_salary = max(max_salary, salary)
+        return max_salary
 
     def get_min_salary(self) -> int:
-        pass
+        min_salary = float("inf")
+        for job in self.jobs_list:
+            if "min_salary" in job and job["min_salary"].isdigit():
+                salary = int(job["min_salary"])
+                min_salary = min(min_salary, salary)
+        return min_salary
+
+    def convert_to_int(self, value: Union[int, str]) -> int:
+        if isinstance(value, (int, str)):
+            try:
+                return int(value)
+            except ValueError:
+                raise ValueError(f"Value {value} is not numeric.")
+        else:
+            raise ValueError(f"Value {value} is not a valid number or string.")
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        if "min_salary" not in job or "max_salary" not in job:
+            raise ValueError("Job does not have salary information")
+
+        min_salary = self.convert_to_int(job["min_salary"])
+        max_salary = self.convert_to_int(job["max_salary"])
+        salary = self.convert_to_int(salary)
+
+        if max_salary < min_salary:
+            raise ValueError("Max salary is less than min salary")
+
+        return min_salary <= salary <= max_salary
 
     def filter_by_salary_range(
-        self, jobs: List[dict], salary: Union[str, int]
+        self, jobs: List[Dict], salary: Union[str, int]
     ) -> List[Dict]:
         pass
